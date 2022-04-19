6d40ce282ac144b55f6a45c6d00a2af59e54c952
Artem Kladov <artem.gen.kladov@gmail.com>
2022-04-19 19:27:44 +0300

    Disable directory listing

--- cmd/v-router/handlers.go
+++ cmd/v-router/handlers.go
@@ -1,196 +1,209 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "github.com/gorilla/mux"
-    log "github.com/sirupsen/logrus"
-    "html/template"
-    "io"
-    "net/http"
-    "os"
-    "path"
-    "regexp"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 // Get some status info
 func statusHandler(w http.ResponseWriter, r *http.Request) {
-    var msg []string
-    status := "ok"
+	var msg []string
+	status := "ok"
 
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
-    err := updateReleasesStatus()
-    if err != nil {
-        msg = append(msg, err.Error())
-        status = "error"
-    }
+	err := updateReleasesStatus()
+	if err != nil {
+		msg = append(msg, err.Error())
+		status = "error"
+	}
 
-    _ = json.NewEncoder(w).Encode(
-        APIStatusResponseType{
-            Status:         status,
-            Msg:            strings.Join(msg, " "),
-            RootVersion:    getRootReleaseVersion(),
-            RootVersionURL: VersionToURL(getRootReleaseVersion()),
-            Releases:       ReleasesStatus.Groups,
-        })
+	_ = json.NewEncoder(w).Encode(
+		APIStatusResponseType{
+			Status:         status,
+			Msg:            strings.Join(msg, " "),
+			RootVersion:    getRootReleaseVersion(),
+			RootVersionURL: VersionToURL(getRootReleaseVersion()),
+			Releases:       ReleasesStatus.Groups,
+		})
 }
 
 // X-Redirect to the stablest documentation version for specific group
 func groupHandler(w http.ResponseWriter, r *http.Request) {
-    var langPrefix string
+	var langPrefix string
 
-    _ = updateReleasesStatus()
-    log.Debugln("Use handler - groupHandler")
+	_ = updateReleasesStatus()
+	log.Debugln("Use handler - groupHandler")
 
-    vars := mux.Vars(r)
-    if len(vars["lang"]) > 0 {
-        langPrefix = fmt.Sprintf("/%s", vars["lang"])
-    }
+	vars := mux.Vars(r)
+	if len(vars["lang"]) > 0 {
+		langPrefix = fmt.Sprintf("/%s", vars["lang"])
+	}
 
-    if version, err := getVersionFromGroup(&ReleasesStatus, vars["group"]); err == nil {
-        log.Debugln(fmt.Sprintf("getVersionFromGroup: Got version - %s for x-redirect", version))
-        w.Header().Set("X-Accel-Redirect", fmt.Sprintf("%s%s/%s/%s", langPrefix, GlobalConfig.LocationVersions, VersionToURL(version), getDocPageURLRelative(r, true)))
-    } else {
-        log.Debugln(fmt.Sprintf("getVersionFromGroup: Got error", version))
-        http.Redirect(w, r, fmt.Sprintf("%s/", langPrefix), 302)
-    }
+	if version, err := getVersionFromGroup(&ReleasesStatus, vars["group"]); err == nil {
+		log.Debugln(fmt.Sprintf("getVersionFromGroup: Got version - %s for x-redirect", version))
+		w.Header().Set("X-Accel-Redirect", fmt.Sprintf("%s%s/%s/%s", langPrefix, GlobalConfig.LocationVersions, VersionToURL(version), getDocPageURLRelative(r, true)))
+	} else {
+		log.Debugln(fmt.Sprintf("getVersionFromGroup: Got error", version))
+		http.Redirect(w, r, fmt.Sprintf("%s/", langPrefix), 302)
+	}
 }
 
 // Handles request to /v<group>-<channel>/. E.g. /v1.2-beta/
 // Temprarily redirect to specific version
 func groupChannelHandler(w http.ResponseWriter, r *http.Request) {
-    var version, URLToRedirect, langPrefix string
-    var err error
+	var version, URLToRedirect, langPrefix string
+	var err error
 
-    log.Debugln("Use handler - groupChannelHandler")
+	log.Debugln("Use handler - groupChannelHandler")
 
-    pageURLRelative := "/"
-    vars := mux.Vars(r)
-    if len(vars["lang"]) > 0 {
-        langPrefix = fmt.Sprintf("/%s", vars["lang"])
-    }
+	pageURLRelative := "/"
+	vars := mux.Vars(r)
+	if len(vars["lang"]) > 0 {
+		langPrefix = fmt.Sprintf("/%s", vars["lang"])
+	}
 
-    _ = updateReleasesStatus()
+	_ = updateReleasesStatus()
 
-    re := regexp.MustCompile(fmt.Sprintf("^/(ru|en)%s/[^/]+/(.+)$", GlobalConfig.LocationVersions))
-    res := re.FindStringSubmatch(r.URL.RequestURI())
-    if res != nil {
-        pageURLRelative = res[2]
-    }
+	re := regexp.MustCompile(fmt.Sprintf("^/(ru|en)%s/[^/]+/(.+)$", GlobalConfig.LocationVersions))
+	res := re.FindStringSubmatch(r.URL.RequestURI())
+	if res != nil {
+		pageURLRelative = res[2]
+	}
 
-    version, err = getVersionFromChannelAndGroup(&ReleasesStatus, vars["channel"], vars["group"])
-    if err == nil {
-        URLToRedirect = fmt.Sprintf("%s%s/%s/%s", langPrefix, GlobalConfig.LocationVersions, VersionToURL(version), pageURLRelative)
-        err = validateURL(fmt.Sprintf("https://%s%s", r.Host, URLToRedirect))
-    }
+	version, err = getVersionFromChannelAndGroup(&ReleasesStatus, vars["channel"], vars["group"])
+	if err == nil {
+		URLToRedirect = fmt.Sprintf("%s%s/%s/%s", langPrefix, GlobalConfig.LocationVersions, VersionToURL(version), pageURLRelative)
+		err = validateURL(fmt.Sprintf("https://%s%s", r.Host, URLToRedirect))
+	}
 
-    if err != nil {
-        log.Errorf("Error validating URL: %v, (original was https://%s/%s)", err.Error(), r.Host, r.URL.RequestURI())
-        notFoundHandler(w, r)
-    } else {
-        http.Redirect(w, r, URLToRedirect, 302)
-    }
+	if err != nil {
+		log.Errorf("Error validating URL: %v, (original was https://%s/%s)", err.Error(), r.Host, r.URL.RequestURI())
+		notFoundHandler(w, r)
+	} else {
+		http.Redirect(w, r, URLToRedirect, 302)
+	}
 }
 
 // Healthcheck handler
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-    _ = json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	_ = json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
 }
 
 // Render templates
 func templateHandler(w http.ResponseWriter, r *http.Request) {
-    if err := updateReleasesStatus(); err != nil {
-        log.Println(err)
-    }
+	if err := updateReleasesStatus(); err != nil {
+		log.Println(err)
+	}
 
-    templateData := templateDataType{
-        VersionItems:           []versionMenuItems{},
-        CurrentGroup:           "", // not used now
-        CurrentChannel:         "",
-        CurrentVersion:         "",
-        CurrentLang:            "",
-        AbsoluteVersion:        "",
-        CurrentVersionURL:      "",
-        CurrentPageURLRelative: "",
-        CurrentPageURL:         "",
-        MenuDocumentationLink:  "",
-    }
+	templateData := templateDataType{
+		VersionItems:           []versionMenuItems{},
+		CurrentGroup:           "", // not used now
+		CurrentChannel:         "",
+		CurrentVersion:         "",
+		CurrentLang:            "",
+		AbsoluteVersion:        "",
+		CurrentVersionURL:      "",
+		CurrentPageURLRelative: "",
+		CurrentPageURL:         "",
+		MenuDocumentationLink:  "",
+	}
 
-    _ = templateData.getVersionMenuData(r)
+	_ = templateData.getVersionMenuData(r)
 
-    tplPath := getRootFilesPath() + r.URL.Path
-    tpl := template.Must(template.ParseFiles(tplPath))
-    err := tpl.Execute(w, templateData)
-    if err != nil {
-        // Should we do some magic here or can simply log error?
-        log.Errorf("Internal Server Error (template error), %s ", err.Error())
-        http.Error(w, "<!-- Internal Server Error (template error) -->", 500)
-    }
+	tplPath := getRootFilesPath() + r.URL.Path
+	tpl := template.Must(template.ParseFiles(tplPath))
+	err := tpl.Execute(w, templateData)
+	if err != nil {
+		// Should we do some magic here or can simply log error?
+		log.Errorf("Internal Server Error (template error), %s ", err.Error())
+		http.Error(w, "<!-- Internal Server Error (template error) -->", 500)
+	}
 }
 
 func serveFilesHandler(fs http.FileSystem) http.Handler {
-    fsh := http.FileServer(fs)
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        upath := r.URL.Path
-        log.Tracef("Serving file " + r.URL.Path)
-        if !strings.HasPrefix(upath, "/") {
-            upath = "/" + upath
-            r.URL.Path = upath
-        }
-        upath = path.Clean(upath)
-        if _, err := os.Stat(fmt.Sprintf("%v%s", fs, upath)); err != nil {
-            if os.IsNotExist(err) {
-                notFoundHandler(w, r)
-                return
-            }
-        }
-        fsh.ServeHTTP(w, r)
-    })
+	fsh := http.FileServer(fs)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		upath := r.URL.Path
+
+		if !strings.HasPrefix(upath, "/") {
+			upath = "/" + upath
+			r.URL.Path = upath
+		}
+
+		upath = path.Clean(upath)
+		fileInfo, err := os.Stat(fmt.Sprintf("%v%s", fs, upath))
+
+		if err != nil {
+			if os.IsNotExist(err) {
+				notFoundHandler(w, r)
+				return
+			}
+		}
+
+		if fileInfo.IsDir() {
+			indexFile := filepath.Join(upath, "index.html")
+			if _, err := os.Stat(fmt.Sprintf("%v%s", fs, indexFile)); err != nil {
+				notFoundHandler(w, r)
+				return
+			}
+		}
+		log.Tracef("Serving file " + r.URL.Path)
+		fsh.ServeHTTP(w, r)
+	})
 }
 
 func rootDocHandler(w http.ResponseWriter, r *http.Request) {
-    var redirectTo, langPrefix string
+	var redirectTo, langPrefix string
 
-    log.Debugln("Use handler - rootDocHandler")
+	log.Debugln("Use handler - rootDocHandler")
 
-    vars := mux.Vars(r)
-    if len(vars["lang"]) > 0 {
-        langPrefix = fmt.Sprintf("/%s", vars["lang"])
-    }
+	vars := mux.Vars(r)
+	if len(vars["lang"]) > 0 {
+		langPrefix = fmt.Sprintf("/%s", vars["lang"])
+	}
 
-    if hasSuffix, _ := regexp.MatchString(fmt.Sprintf("^/[^/]+%s/.+", GlobalConfig.LocationVersions), r.RequestURI); hasSuffix {
-        items := strings.Split(r.RequestURI, fmt.Sprintf("%s/", GlobalConfig.LocationVersions))
-        if len(items) > 1 {
-            if is_version_or_Channel, _ := regexp.MatchString(fmt.Sprintf("^(%s|v[0-9]+.[0-9]+.[0-9]+([^/]+)?)[/]?", channelList), items[1]); is_version_or_Channel {
-                // We can't handle requests to specific version. They should be routed by balancer (create corresponding Ingress resource)
-                serveFilesHandler(http.Dir(getRootFilesPath())).ServeHTTP(w, r)
-            }
-            redirectTo = strings.Join(items[1:], fmt.Sprintf("%s%s/", langPrefix, GlobalConfig.LocationVersions))
-        }
-    }
+	if hasSuffix, _ := regexp.MatchString(fmt.Sprintf("^/[^/]+%s/.+", GlobalConfig.LocationVersions), r.RequestURI); hasSuffix {
+		items := strings.Split(r.RequestURI, fmt.Sprintf("%s/", GlobalConfig.LocationVersions))
+		if len(items) > 1 {
+			if is_version_or_Channel, _ := regexp.MatchString(fmt.Sprintf("^(%s|v[0-9]+.[0-9]+.[0-9]+([^/]+)?)[/]?", channelList), items[1]); is_version_or_Channel {
+				// We can't handle requests to specific version. They should be routed by balancer (create corresponding Ingress resource)
+				serveFilesHandler(http.Dir(getRootFilesPath())).ServeHTTP(w, r)
+			}
+			redirectTo = strings.Join(items[1:], fmt.Sprintf("%s%s/", langPrefix, GlobalConfig.LocationVersions))
+		}
+	}
 
-    http.Redirect(w, r, fmt.Sprintf("%s%s/%s/%s", langPrefix, GlobalConfig.LocationVersions, GlobalConfig.DefaultGroup, redirectTo), 301)
+	http.Redirect(w, r, fmt.Sprintf("%s%s/%s/%s", langPrefix, GlobalConfig.LocationVersions, GlobalConfig.DefaultGroup, redirectTo), 301)
 }
 
 // Redirect to root documentation if request not matches any location (override 404 response)
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-    lang := "en"
+	lang := "en"
 
-    re := regexp.MustCompile(`^/(ru|en)/.*$`)
-    res := re.FindStringSubmatch(r.URL.RequestURI())
-    if res != nil {
-        lang = res[1]
-    }
+	re := regexp.MustCompile(`^/(ru|en)/.*$`)
+	res := re.FindStringSubmatch(r.URL.RequestURI())
+	if res != nil {
+		lang = res[1]
+	}
 
-    w.WriteHeader(http.StatusNotFound)
-    page404File, err := os.Open(fmt.Sprintf("%s/%s/404.html", getRootFilesPath(), lang))
-    defer page404File.Close()
-    if err != nil {
-        // 404.html file not found! Send the fallback page...
-        log.Error("404.html file not found")
-        http.Error(w, `<html lang="en">
+	w.WriteHeader(http.StatusNotFound)
+	page404File, err := os.Open(fmt.Sprintf("%s/%s/404.html", getRootFilesPath(), lang))
+	defer page404File.Close()
+	if err != nil {
+		// 404.html file not found! Send the fallback page...
+		log.Error("404.html file not found")
+		http.Error(w, `<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -214,7 +227,7 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 </div>
 </body>
 </html>`, 404)
-        return
-    }
-    io.Copy(w, page404File)
+		return
+	}
+	io.Copy(w, page404File)
 }
